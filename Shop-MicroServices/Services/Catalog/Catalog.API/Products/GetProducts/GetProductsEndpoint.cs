1695523c1f890f5ae52974d00c5c13a5f61c2e9b
Salim Jarmakani <salimjar01@gmail.com>
2024-08-13 01:11:33 +0100

    added Get Products minimal Api

---
+++ Shop-MicroServices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsResponse(IEnumerable<Product> products);
+public class GetProductsEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("/products", async(ISender sender) =>
+		{
+			var result = await sender.Send(new GetProductsQuery());
+
+			GetProductsResponse response = result.Adapt<GetProductsResponse>();
+
+			return Results.Ok(response);
+			
+		}).WithName("GetProducts")
+		.WithDescription("Gets All Products")
+		.ProducesProblem(StatusCodes.Status400BadRequest);
+	}
+}
---
+++ Shop-MicroServices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsResult(IEnumerable<Product> Products);
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+internal class GetProductsHandler(IDocumentSession session,ILogger<GetProductsHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+	public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+	{
+		IEnumerable<Product> products = await session.Query<Product>().ToListAsync(token: cancellationToken);
+
+		return new GetProductsResult(products);
+	}
+}
