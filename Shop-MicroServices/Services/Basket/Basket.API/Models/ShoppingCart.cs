9c7824701dfc39c379e4885010ad134993624454
Salim Jarmakani <salimjar01@gmail.com>
2024-09-28 19:57:21 -0400

    created structure for Baske.API mircroservice

---
+++ Shop-MicroServices/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    //Mapping Constructor
+    public ShoppingCart() { }
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+}
