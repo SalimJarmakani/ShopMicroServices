c1e3ce01dff026b41ae038e859f2f181eae97300
Salim Jarmakani <salimjar01@gmail.com>
2025-05-06 00:53:08 -0400

    worked on value objects

--- Shop-MicroServices/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ Shop-MicroServices/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -11,6 +11,7 @@ public class Order: Aggregate<OrderId>
 
     public OrderName OrderName {  get; private set; } = default!;
     public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
     public Payment Payment { get; private set; } = default!;
     public OrderStatus Status { get; private set; } = OrderStatus.Pending;
 
--- Shop-MicroServices/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ Shop-MicroServices/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -2,5 +2,16 @@
 
 public record OrderName
 {
+    private const int DefaultLength = 5;
     public string Value { get;}
+    private OrderName(string value) => Value = value;
+
+
+    private static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+
+        return new OrderName(value);
+    }
 }
