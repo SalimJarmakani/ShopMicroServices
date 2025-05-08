0e440976a25feffcfe07b0ee6ec141d9473bd362
Salim Jarmakani <salimjar01@gmail.com>
2025-05-07 20:23:37 -0400

    worked on domain objects

---
+++ Shop-MicroServices/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItem.Configuration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+internal class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        
+    }
+}
