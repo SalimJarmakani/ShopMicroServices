dc92e5b3b7584d8fb4f2c4180048f480a7f3df41
Salim Jarmakani <salimjar01@gmail.com>
2025-05-20 00:07:44 -0400

    worked on Application layer

---
+++ Shop-MicroServices/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Ordering.Domain.Models;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Data;
+global using Core.Services.CQRS;
+global using Ordering.Domain.ValueObjects;
