579baf215313b8e04d5723028fcb5b3e8be5d62a
Salim Jarmakani <salimjar01@gmail.com>
2025-05-25 23:50:33 -0400

    worked on messaging and finished up api

---
+++ Shop-MicroServices/Core.Services.Messaging/IntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Services.Messaging;
+
+public record IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+
+    public DateTime OccuredOn => DateTime.Now;
+
+    public string EventType => GetType().AssemblyQualifiedName;
+}
--- Shop-MicroServices/Core.Services/Pagination/PaginatedResult.cs
+++ Shop-MicroServices/Core.Services/Pagination/PaginatedResult.cs
@@ -4,8 +4,8 @@ public class PaginatedResult<TEntity>(int pageIndex,int pageSize,long count,IEnu
     where TEntity : class
 {
 
-    public int PageIndex { get; set; }
-    public int PageSize { get; set; } = pageSize;
-    public long Count { get; set; }
+    public int PageIndex { get; } = pageIndex;
+    public int PageSize { get;  } = pageSize;
+    public long Count { get; } = count;
     public IEnumerable<TEntity> Data { get; } = data;
 }
