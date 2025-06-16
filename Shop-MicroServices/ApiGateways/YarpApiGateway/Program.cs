fb04a83adad335a05486291df002b763c7584529
Salim Jarmakani <salimjar01@gmail.com>
2025-06-15 22:54:12 -0400

    wrapped up YARP and working on Web App

--- Shop-MicroServices/ApiGateways/YarpApiGateway/Program.cs
+++ Shop-MicroServices/ApiGateways/YarpApiGateway/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services To Container
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
