59952c0acf4e1320b31aec422cf204bf7ba2a102
Salim Jarmakani <salimjar01@gmail.com>
2025-06-18 22:16:18 -0400

    worked on Razor Pages

--- Shop-MicroServices/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
+++ Shop-MicroServices/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -5,6 +5,8 @@ public class ShoppingCartModel
     public string UserName { get; set; } = default!;
 
     public List<ShoppingCartItemModel> Items { get; set; } = new();
+
+    public decimal TotalPrice => Items.Sum(x=> x.Price); 
 }
 
 
--- Shop-MicroServices/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ Shop-MicroServices/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,22 +1,48 @@
-namespace Shopping.Web.Pages
+namespace Shopping.Web.Pages;
+
+public class IndexModel(
+    ICatalogService catalogService,
+    IBasketService basketService
+    ,ILogger<IndexModel> logger) 
+    : PageModel
 {
-    public class IndexModel(ICatalogService catalogService,ILogger<IndexModel> logger) 
-        : PageModel
+
+    public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
+
+    public async Task<IActionResult> OnGetAsync()
     {
+        logger.LogInformation("Index Page Visited");
 
-        public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
+        //var result = await catalogService.GetProducts();
+        var result = await catalogService.GetProducts();
 
-        public async Task<IActionResult> OnGetAsync()
-        {
-            logger.LogInformation("Index Page Visited");
+        ProductList = result.Products;
 
-            //var result = await catalogService.GetProducts();
-            var result = await catalogService.GetProducts(2,3);
+        return Page();
 
-            ProductList = result.Products;
-
-            return Page();
-
-        }
     }
+
+    public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+    {
+        logger.LogInformation("Add to cart button clicked");
+
+        var productResponse = await catalogService.GetProduct(productId);
+
+        var basket = await basketService.LoadUserBasket();
+
+        basket.Items.Add(new ShoppingCartItemModel
+        {
+            ProductId = productId,
+            ProductName= productResponse.Product.Name,
+            Price = productResponse.Product.Price,
+            Quantity = 1,
+            Color="Black"
+        });
+
+        await basketService.StoreBasket(new StoreBasketRequest(basket));
+
+        return RedirectToPage("Cart");
+    }
+
+
 }
